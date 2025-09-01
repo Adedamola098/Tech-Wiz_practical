b2ff3c2001f08a05799cb43979cbe125db1686f0
Adedamola098 <ayomikunadegboyega@gmail.com>
2025-09-01 06:24:34 -0700

    udh

--- pages/urls.py
+++ pages/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='dashboard'),
-    path('about/',)
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.Contact, name='contact'),
 
     
 
