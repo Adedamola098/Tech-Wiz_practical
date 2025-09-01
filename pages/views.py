f71120a3ce7f44956b5748ddba5b71e1813f8fbe
Naruto-solo <johnsonafolabi017@gmail.com>
2025-09-01 14:13:49 +0100

    ok

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(requests):
-<<<<<<< HEAD
     return render(requests, 'pages/Home.html')
 
 def about(requests):
@@ -10,7 +9,3 @@ def about(requests):
 
 def Contact(requests):
     return render(requests, 'pages/Contact.html')
-=======
-    return render(requests, 'pages/home.html')
-   
->>>>>>> 4ef053d5deb0f34dc12aff003d38d0b86cff1c37
